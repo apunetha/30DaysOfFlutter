88a4e0691fc85b81eb7689f9a797ab3ab62b4fd6
aman punetha <amanpunethagehu@gmail.com>
2021-09-16 21:55:08 +0530

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  final int days = 30;
+  final String name = "Aman";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
