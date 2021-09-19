778dc003f0078eb2079df5f4064565e9631a39bd
aman punetha <amanpunethagehu@gmail.com>
2021-09-19 23:54:05 +0530

    day 11 with 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,15 @@
+class CatalogModel{
+ static final items = [
+  Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000")
+];
+}
 class Item {
   final num id;
   final String name;
@@ -15,13 +27,4 @@ class Item {
       required this.image});
 }
 
-final products = [
-  Item(
-      id: 1,
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000")
-];
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,16 +9,19 @@ class HomePage extends StatelessWidget {
   final String name = "Aman";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // ignore: prefer_const_constructors
         title: Text("Catalog App"),
       ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: Text("Welcome to $days days flutter by $name"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       // ignore: prefer_const_constructors
       drawer: MyDrawer(),
